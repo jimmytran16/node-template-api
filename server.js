756499176a370cc90ba1981d55c20d2fecf192da
jimmytran16 <jimmytran1620@gmail.com>
2022-01-09 20:38:13 -0500

    updates to Procfile and PORT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = require('./src/routes/homeRouter');
 const app = express();
 const routers = require('./src/routes/index')
+const port = process.env.PORT || 4000;
 
 app.use(express.json())
 app.use(express.urlencoded())
@@ -10,4 +11,4 @@ app.use(require('morgan')('dev'))
 app.use('/', routers.homeRouter);
 app.use('/test', routers.testRouter);
 
-app.listen(4000, () => console.log(`Connection to PORT ${ 4000 }`));
+app.listen(port, () => console.log(`Connection to PORT ${ 4000 }`));
